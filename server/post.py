302080dfe146440ba36f988c6253450a42f2a296
yafeng <yafengabc@gmail.com>
2015-06-30 14:30:46 +0800

    new file:   server/post.py 	new file:   server/test.db

---
+++ server/post.py
@@ -0,0 +1,37 @@
+from bottle import post,request,run,debug,route
+import json
+import sqlite3
+
+def insertdb(dat):
+    print("open the datebase")
+    db=sqlite3.connect("test.db")
+    print("insert into database")
+    print (dat)
+    dat=dat.replace("'",'"')
+    dat=json.loads(dat)
+    print (dat)
+    db.execute("insert into pimark values(?,?,?,?,?,?)",\
+            (dat["CPU"],dat["HW"],dat["kernel"],dat["GCC"],dat["PIC"],dat["GMPI"]))
+    db.commit()
+    pass
+
+def makehtml():
+    pass
+
+@route('/')
+def index():
+    a="asd"
+    b="ddd"
+    return "hello kitty ? ?"
+
+@post('/send')
+def postd():
+    data=request.body
+    datas=data.read().decode('utf-8')
+    print(datas)
+    insertdb(datas)
+    makehtml()
+    return "Post OK\n" 
+
+run(server="paste",host="0.0.0.0",debug=True)
+#run(host="0.0.0.0",debug=True)
